f3127c3ce62bbac6d647c25edfa0dc95a038ecde
Regional PBT <regionalpbt@outlook.com>
2025-09-15 15:01:53 +0800

    09.15.2025 No-pay & casual leave checking fix bug

--- my_app/eleave/views.py
+++ my_app/eleave/views.py
@@ -1782,20 +1782,20 @@ def checkConsecutive(racf, year, apply_h, type, office):
 
                 if r['Applied'] == "LVE02" or r['Apply'] == "LVE02":
                     casual_count += 0.5
-                elif (r['Day of Week'] == "Saturday" or r['Day of Week'] == "Sunday") or 'LVE01' in r['Applied']:
+                elif (r['Day of Week'] == "Saturday" or r['Day of Week'] == "Sunday") or r['Applied'] != "":
                     casual_count = casual_count
                 else:
                     casual_count = 0
 
                 if casual_count > 3:
-                    return ({"consecutive": True, "error_message" : "Reminder: Casual leave cannot be applied for 4 or more consecutive days", "result": None,  "Status_code": 506, "no_of_consective": sl_count})
+                    return ({"consecutive": True, "error_message" : "Reminder: Casual leave cannot be applied for more than 3 consecutive days", "result": None,  "Status_code": 506, "no_of_consective": sl_count})
 
             # No pay leave cannot exceed 5 working days
             if type == "LVE06":
                 if (r['Day of Week'] == "Saturday" or r['Day of Week'] == "Sunday") or (r['Applied'] != "" and 'LVE' not in r['Applied']):
                     count -= 0.5
                 if count > 5:
-                    return ({"consecutive": True, "error_message" : "Reminder: No pay leave cannot exceed 5 consecutive working days, please contact HR", "result": None,  "Status_code": 506, "no_of_consective": sl_count})
+                    return ({"consecutive": True, "error_message" : "Reminder: No pay taken at any one time is 1 WEEK including Public Holidays, Saturdays and Sundays", "result": None,  "Status_code": 506, "no_of_consective": sl_count})
 
             # Consider consecutive weekends only if a leave has been applied before
             if r['Day of Week'] == "Saturday" and rdf.iloc[i-1]['Applied'] == "" and rdf.iloc[i-1]['Apply'] == "":
